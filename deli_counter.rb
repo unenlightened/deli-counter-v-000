acf4d04ed42aa4741def77c8a247efc49ea0cd7a
Chely Ho <adept_inept@yahoo.com>
2018-02-07 00:31:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ katz_deli = []
 def line(customers)
   if customers.length == 0
     puts "The line is currently empty."
-  end
+  else
+    puts "The line is currently:"
+    customers.each_with_index do |customer, index|
+      print " #{index+1}. #{customer}"
+
 end
 
 def take_a_number(customers, new_customer)
