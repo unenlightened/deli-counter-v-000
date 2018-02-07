ae4a5575a57879c700c321fb3d6c6371c40b5b6a
Chely Ho <adept_inept@yahoo.com>
2018-02-07 00:28:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,11 @@
 # Write your code here.
 katz_deli = []
 
-def line(array)
-  if array.length == 0
+def line(customers)
+  if customers.length == 0
     puts "The line is currently empty."
   end
+end
+
+def take_a_number(customers, customer)
 end
