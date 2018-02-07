5ed7195b1fc55435908024d34ddd5fbb0b46c0e6
Chely Ho <adept_inept@yahoo.com>
2018-02-07 00:50:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(customers)
     puts "The line is currently empty."
   else
     customers.each_with_index.collect do |customer, index|
-      "#{index+1}. #{customer}"
+      "1 #{customer}"
     end
     puts "The line is currently: #{customers}"
   end
